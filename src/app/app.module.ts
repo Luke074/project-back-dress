72a71d928bcdbdb119041f8af3f1f2e2af6e3e68
Lucas Alves <68557804+Luke074@users.noreply.github.com>
2022-07-24 09:12:37 -0300

    feat: slide-show

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { SliderShowComponent } from './components/slider-show/slider-show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SliderShowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/slider-show/slider-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-show',
+  templateUrl: './slider-show.component.html',
+  styleUrls: ['./slider-show.component.scss']
+})
+export class SliderShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
